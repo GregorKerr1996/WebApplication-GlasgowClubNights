fab534f5ebec699646bfeb37cb83a3840ad1d0ce
Gregor <2139619k@student.gla.ac.uk>
2018-03-07 16:15:01 +0000

    Main Pages added, Relative links, Graphic Updates

--- gcn/urls.py
+++ gcn/urls.py
@@ -3,4 +3,26 @@ from gcn import views
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
+
+    url(r'^about_us/$', views.about_us, name='about_us'),
+    url(r'^contact_us/$', views.contact_us, name='contact_us'),
+    url(r'^club_list/$', views.club_list, name='club_list'),
+    url(r'^login/$', views.login, name='login'),
+
+    url(r'^bamboo/$', views.bamboo, name='bamboo'),
+    url(r'^cathouse/$', views.cathouse, name='cathouse'),
+    url(r'^firewater/$', views.firewater, name='firewater'),
+    url(r'^garage/$', views.garage, name='garage'),
+    url(r'^hive/$', views.hive, name='hive'),
+    url(r'^kokomo/$', views.kokomo, name='kokomo'),
+    url(r'^kushion/$', views.kushion, name='kushion'),
+    url(r'^la_cheetah/$', views.la_cheetah, name='la_cheetah'),
+    url(r'^mango/$', views.mango, name='mango'),
+    url(r'^polo/$', views.polo, name='polo'),
+    url(r'^sanctuary/$', views.sanctuary, name='sanctuary'),
+    url(r'^shimmy/$', views.shimmy, name='shimmy'),
+    url(r'^subclub/$', views.subclub, name='subclub'),
+    url(r'^swg3/$', views.swg3, name='swg3'),
+    url(r'^viper/$', views.viper, name='viper'),
+
 ]
--- gcn/views.py
+++ gcn/views.py
@@ -7,3 +7,80 @@ from django.http import HttpResponse
 def home(request):
 
     return render(request, 'glasgowclubnights/home.html')
+
+def about_us(request):
+
+    return render(request, 'glasgowclubnights/about_us.html')
+
+def contact_us(request):
+
+    return render(request, 'glasgowclubnights/contact_us.html')
+
+def club_list(request):
+
+    return render(request, 'glasgowclubnights/club_list.html')
+
+def login(request):
+
+    return render(request, 'glasgowclubnights/login.html')
+
+def bamboo(request):
+
+    return render(request, 'glasgowclubnights/club_list/bamboo.html')
+
+def cathouse(request):
+
+    return render(request, 'glasgowclubnights/club_list/cathouse.html')
+
+def firewater(request):
+
+    return render(request, 'glasgowclubnights/club_list/firewater.html')
+
+def garage(request):
+
+    return render(request, 'glasgowclubnights/club_list/garage.html')
+
+def hive(request):
+
+    return render(request, 'glasgowclubnights/club_list/hive.html')
+
+def kokomo(request):
+
+    return render(request, 'glasgowclubnights/club_list/kokomo.html')
+
+def kushion(request):
+
+    return render(request, 'glasgowclubnights/club_list/kushion.html')
+
+def la_cheetah(request):
+
+    return render(request, 'glasgowclubnights/club_list/la_cheetah.html')
+
+def mango(request):
+
+    return render(request, 'glasgowclubnights/club_list/mango.html')
+
+def polo(request):
+
+    return render(request, 'glasgowclubnights/club_list/polo.html')
+
+def sanctuary(request):
+
+    return render(request, 'glasgowclubnights/club_list/sanctuary.html')
+
+def shimmy(request):
+
+    return render(request, 'glasgowclubnights/club_list/shimmy.html')
+
+def subclub(request):
+
+    return render(request, 'glasgowclubnights/club_list/subclub.html')
+
+def swg3(request):
+
+    return render(request, 'glasgowclubnights/club_list/swg3.html')
+
+def viper(request):
+
+    return render(request, 'glasgowclubnights/club_list/viper.html')
+
